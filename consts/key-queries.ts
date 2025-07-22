a19fda92353c28f8288318d9e8defb8d9427c419
kevinkeyssx <keyss1700@gmail.com>
2025-07-21 21:27:30 -0400

    KEY_QUERYS added

---
+++ consts/key-queries.ts
@@ -0,0 +1,11 @@
+export const KEY_QUERYS = {
+    REQUESTS        : 'requests',
+    REQUEST_DETAIL  : 'request-detail',
+
+    SIZE            : 'sizes',
+    DAYS            : 'days',
+    SPACES          : 'spaces',
+    MODULES         : 'modules',
+
+    COST_CENTERS    : 'cost-centers',
+}
