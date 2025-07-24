ce910dfd10716448de9f1297bccc4eea9bb1758d
kevinkeyssx <keyss1700@gmail.com>
2025-07-23 22:18:51 -0400

    Fixed imports

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,9 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue }    from 'clsx';
+import { twMerge }                  from 'tailwind-merge';
+import { format }                   from "@formkit/tempo"
 
-import { Level, SpaceType, Status } from '@/types/request';
-
-import { format } from "@formkit/tempo"
-
+import { Status } from '@/types/request';
+import { SpaceType, Level } from '@/types/request-detail.model';
 
 
 export function cn(...inputs: ClassValue[]) {
