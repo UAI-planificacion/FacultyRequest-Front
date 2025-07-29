ad8ab567b4dcf1ec4a968f3c5ec9a56f6cb9bad7
kevinkeyssx <keyss1700@gmail.com>
2025-07-28 20:26:21 -0400

    Comment model added

---
+++ types/comment.model.ts
@@ -0,0 +1,29 @@
+export interface StaffComment {
+    name    : string;
+    email   : string;
+}
+
+
+export interface Comment {
+    id          : string;
+    content     : string;
+    staff       : StaffComment | null;
+    adminName   : string | null;
+    adminEmail  : string | null;
+    createdAt   : Date | string;
+    updatedAt   : Date | string;
+}
+
+
+export interface CreateComment {
+    content             : string;
+    requestId?          : string | null | undefined;
+    requestDetailId?    : string | null | undefined;
+    staffId             : string;
+}
+
+
+export interface UpdateComment {
+    id      : string;
+    content : string;
+}
