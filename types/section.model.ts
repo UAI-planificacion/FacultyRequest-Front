57d8ede1cd69e78a5229b5a80ded14f495ed0033
kevinkeyssx <keyss1700@gmail.com>
2025-09-23 22:26:58 -0300

    Section model implemented

--- types/section.model.ts
+++ types/section.model.ts
@@ -1,21 +1,117 @@
 import { Size } from "@/types/request-detail.model";
 
 
-export interface Section {
-    id                      : string,
-    code                    : number,
-    session                 : string,
-    size                    : Size,
-    correctedRegistrants    : number,
-    realRegistrants         : number,
-    plannedBuilding         : string,
-    chairsAvailable         : number,
-    room                    : string,
-    professorName           : string,
-    professorId             : string,
-    day                     : number,
-    moduleId                : string,
-    subjectName             : string,
-    subjectId               : string,
-    period                  : string
+interface Day {
+    id      : number;
+    name    : string;
+}
+
+
+interface Module {
+    id          : number;
+    code        : string;
+    startHour   : string;
+    endHour     : string;
+    diference   : string | null;
+}
+
+
+interface Professor {
+    id      : string;
+    name    : string;
+}
+
+
+interface Subject {
+    id      : string;
+    name    : string;
+}
+
+
+interface Period {
+    id      : string;
+    name    : string;
+}
+
+
+export interface SectionBase {
+    size?                   : Size | null | undefined;
+    correctedRegistrants?   : number | null;
+    realRegistrants?        : number | null;
+    plannedBuilding?        : string | null;
+    chairsAvailable?        : number | null;
+}
+
+
+export interface Section extends SectionBase {
+    id          : string;
+    code        : number;
+    period      : Period;
+    subject     : Subject;
+    room        : string    | null;
+    professor   : Professor | null;
+    isClosed    : boolean;
+    groupId     : string;
+    day         : Day       | null;
+    module      : Module    | null;
+    session     : Session;
+
+}
+
+interface SectionSave {
+    roomId?         : string | null;
+    professorId?    : string | null;
+    dayModuleId?    : number | null;
+
+}
+
+
+export interface CreateSectionRequest extends SectionBase, SectionSave {
+    periodId    : string;
+    subjectId   : string;
+    code        : number;
+    groupId     : string;
+    session     : Session;
+}
+
+
+export interface UpdateSectionRequest extends SectionBase, SectionSave {
+    id      : string;
+    session : Session;
+
+}
+
+
+export interface UpdateMassiveSectionRequest extends SectionBase, SectionSave {
+    session? : Session | null;
+}
+
+
+export enum Session {
+    C = 'C', // Cátedra
+    A = 'A', // Ayudantía
+    T = 'T', // Taller
+    L = 'L'  // Laboratorio
+}
+
+export interface SectionToCreate {
+    periodId    : string;
+    session     : Session;
+    code        : number;
+    groupId     : string;
+}
+
+export interface SessionCounts {
+    [Session.C]: number;
+    [Session.A]: number;
+    [Session.T]: number;
+    [Session.L]: number;
+}
+
+export interface SectionData {
+    id              : string;
+    period          : string;
+    sessionCounts   : SessionCounts;
+    sectionNumber   : number;
+    isNew           : boolean;
 }
