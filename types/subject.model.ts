a5b12efb030868bfae43329f437e45806bcbdff7
kevinkeyssx <keyss1700@gmail.com>
2025-12-18 23:05:18 -0300

    Subject model updated

--- types/subject.model.ts
+++ types/subject.model.ts
@@ -1,26 +1,69 @@
-import { Size, SpaceType } from "@/types/request-detail.model";
+import { BuildingEnum, Size, SpaceType } from "@/types/request-detail.model";
+
+
+interface Faculty {
+    id      : string;
+    name    : string;
+}
+
 
 interface BaseSubject {
     id              : string;
     name            : string;
-    costCenterId    : string;
     spaceSizeId     : Size      | null;
     spaceType       : SpaceType | null;
+    workshop        : number;
+    lecture         : number;
+    tutoringSession : number;
+    laboratory      : number;
+    quota           : number | null;
+    // faculty         : Faculty;
+}
+
+
+interface Grade {
+    id              : string;
+    name            : string;
+    headquartersId  : string;
 }
 
 
 export interface Subject extends BaseSubject {
     facultyId   : string;
     isActive    : boolean;
-    createdAt   : Date | string;
-    updatedAt   : Date | string;
+    createAt    : Date      | string;
+    updateAt    : Date      | string;
     offersCount : number;
+    grade       : Grade | null;
+    faculty         : Faculty;
 }
 
 
 export interface CreateSubject extends BaseSubject {
-    facultyId: string;
+    facultyId   : string;
+    gradeId     : string | null;
 }
 
 
-export interface UpdateSubject extends Partial<BaseSubject> {}
+export interface UpdateSubject extends Partial<BaseSubject> {
+    gradeId?    : string | null;
+    isActive?   : boolean;
+}
+
+
+export interface CreateOfferSubject {
+	subjectId       : string;
+	periodId        : string;
+	professorId     : string | null;
+	numberOfSections: number;
+	spaceType       : string | null;
+	spaceSizeId     : string | null;
+	building        : BuildingEnum | null;
+	workshop        : number;
+	lecture         : number;
+	tutoringSession : number;
+	laboratory      : number;
+	startDate       : Date;
+	endDate         : Date;
+    quota           : number;
+}
