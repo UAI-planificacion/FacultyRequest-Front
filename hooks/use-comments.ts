ba0ec2ccd25aa858189c7ce3c2a1b5306ad6125b
kevinkeyssx <keyss1700@gmail.com>
2025-12-22 21:57:50 -0300

    CommentType & useComments updated

--- hooks/use-comments.ts
+++ hooks/use-comments.ts
@@ -34,14 +34,14 @@ interface UseCommentsReturn {
 
 
 export enum CommentType {
-    REQUEST = "request",
-    DETAIL = 'request-detail'
+    REQUEST_SESSION = "request-sessions",
+    PLANNING_CHANGE = 'planning-change'
 }
 
 
-interface CommentsProps {
-    requestId?          : string;
-	requestDetailId?    : string;
+interface Props {
+    requestSessionId?   : string;
+	planningChangeId?   : string;
     enabled             : boolean;
 }
 
@@ -53,17 +53,17 @@ interface CommentsProps {
  * @returns Object containing comments data and management functions
  */
 export function useComments({
-    requestId,
-    requestDetailId,
+    requestSessionId,
+    planningChangeId,
     enabled = true,
-}: CommentsProps ): UseCommentsReturn {
-	const queryClient   = useQueryClient();
-    const id            = requestId || requestDetailId;
+}: Props ): UseCommentsReturn {
+	const queryClient = useQueryClient();
+    const id = requestSessionId || planningChangeId;
 
     if ( !id ) throw new Error ( 'Id is required' );
 
-    const type      = requestId ? CommentType.REQUEST : CommentType.DETAIL;
-    const endpoint  = 'comments';
+    const type = requestSessionId ? CommentType.REQUEST_SESSION : CommentType.PLANNING_CHANGE;
+    const endpoint = 'comments';
 
 	const {
 		data: comments = [],
@@ -89,6 +89,7 @@ export function useComments({
             url     : `${endpoint}/${commentId}`,
             method  : Method.DELETE
         });
+
 
 
     const updateCommentApi = async ( updateComment: UpdateComment ): Promise<Comment>  =>
